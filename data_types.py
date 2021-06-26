a3f97c55cd39b940ade19b461533b72e693f353e
tanveerahmed98 <tanveer.ahm98@gmail.com>
2021-06-26 17:56:15 +0500

    Learning Python

---
+++ data_types.py
@@ -0,0 +1,24 @@
+# Strings
+
+a = "Tanveer"
+b = 'TA'
+
+# Numbers
+
+age = 23
+weight = 54.4
+productivity_score = .97
+
+# Lists
+
+weights = [55, 65, 75, 80]
+
+# Dictionary
+
+book = {
+    "title"  : "How to be awesome",
+    "author" : "John Doe",
+    "date_published" : "26-6-2021",
+}
+
+print(book)
---
+++ variables.py
@@ -0,0 +1,8 @@
+# Always use # sign to add comment in the code.
+x = 2
+name = "john"
+q = 2.3
+age = 21
+first_name = "Tanveer"
+print(first_name)
+print("My name is Tanveer Ahmed")
